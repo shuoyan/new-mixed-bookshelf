3ca846cb0c7fdffc14b081810f83251c2c7cf687
shuoyan <yanshuobit@gmail.com>
2014-03-13 14:28:50 -0400

    change name app.json

---
+++ myScript.js
@@ -0,0 +1,57 @@
+$(function() {
+    $( "#indesigncs3" ).draggable();
+  });
+  
+$(function() {
+    $( "#citadelspirit" ).draggable();
+  });
+  
+$(function() {
+    $( "#notsowild" ).draggable();
+  });
+
+
+
+
+$(function() {
+    $( "#thedevotedlife" ).draggable();
+  }); 
+  
+
+
+$(function() {
+    $( "#slowlooking" ).draggable();
+  });
+
+
+$(function() {
+    $( "#thebrain" ).draggable();
+  });
+
+$(function() {
+    $( "#girl" ).draggable();
+  });
+
+$(function() {
+    $( "#music" ).draggable();
+  });
+
+$(function() {
+    $( "#choosing" ).draggable();
+  });
+
+$(function() {
+    $( "#nothing" ).draggable();
+  });
+
+$(function() {
+    $( "#persuasion" ).draggable();
+  });
+
+$(function() {
+    $( "#enterprise" ).draggable();
+  });
+
+$(function() {
+    $( "#iainpears" ).draggable();
+  }); 
