6793509870df245939009e381f14807edfd8ade0
shuoyan <yanshuobit@gmail.com>
2014-03-28 22:17:39 -0400

    index app.js change

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
-function($,              _,                DOMUtils,
-         Embed) {
-
-    "use strict";
-
-    var enabled = true;
-
-    var touchIndicatorDiv = $("<div class='lens-touch-indicator' />");
-
-    var TouchIndicator = /** @lends Lens.LAF.TouchIndicator */ {
-        /**
-         * Enables scrollbar styling
-         */
-        enable: function() {
-            DOMUtils.addStylesheet("LAF/widgets/touch_indicator.css");
-            enabled = true;
-        }
-        
-
 Lens.init(["Components/draggable"], function() {
     "use strict";
 
     var $ = Lens.Libs.jQuery;
 
     var indesigncs3 = $("#indesigncs3");
+    var citadelspirit = $("#citadelspirit");
+    var music = $("music");
 
-    Lens.Components.Draggable(indesigncs3);
+    Lens.Components.Draggable(indesigncs3,false);
+    Lens.Components.Draggable(citadelspirit,false);
+    Lens.Components.Draggable(music,false);
 
     indesigncs3[0].addEventListener("lens:touchstart", function(evt) {
         var touches = evt.targetTouches;
-        
-
-        touches.ended(function() {
-            
+        touches.ended(function() {    
         });
     });
+
+    citadelspirit[0].addEventListener("lens:touchstart", function(evt) {
+        var touches = evt.targetTouches;
+        touches.ended(function() {    
+        });
+    });
+
+    music[0].addEventListener("lens:touchstart", function(evt) {
+        var touches = evt.targetTouches;
+        touches.ended(function() {    
+        });
+    });
+
 });
 
 
